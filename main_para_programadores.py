a029f0080e8474281131c85732b752259fc75b24
Viktor Y. D. Sumida <viktor.sumida@alumni.usp.br>
2022-12-15 16:44:31 -0300

    Testes

--- main_para_programadores.py
+++ main_para_programadores.py
@@ -323,7 +323,8 @@ li = [0.] * quantidade  # vetor longitude manchas
 
 
 tempSpot = 0.418 * tempStar + 1620 # Temp. calculada em Rackham et al. 2018 p/ estrelas do tipo F-G-K
-#tempSpot = 3000
+#tempSpot = 3500
+#print("Tempurature: ", tempSpot)
 
 intensidadeMancha = np.zeros(num_elements)
 intensidadeManchaNormalizada = np.zeros(num_elements)
