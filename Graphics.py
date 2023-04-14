3239f584fe40a57c44744bc9267c9d7cc86c563d
Viktor Y. D. Sumida <viktor.sumida@alumni.usp.br>
2023-04-13 23:53:32 -0300

    Modificações para o paper

--- Graphics.py
+++ Graphics.py
@@ -27,7 +27,7 @@ import seaborn as sns
 
 fig = plt.figure()
 
-graph = 5
+graph = 3
 
 if (graph == 1):
 
@@ -154,10 +154,10 @@ elif (graph == 3):
 
     # Leitura de entrada dos dados
 
-    transit_depth_phot_trans_lat_0 = np.genfromtxt("output_transit_depth(trans_lat=0graus,f_spot=0.0).txt", delimiter=",")
-    transit_depth_phot_trans_lat_20 = np.genfromtxt("output_transit_depth(trans_lat=-20graus,f_spot=0.0).txt", delimiter=",")
-    transit_depth_phot_trans_lat_40 = np.genfromtxt("output_transit_depth(trans_lat=-40graus,f_spot=0.0).txt", delimiter=",")
-    transit_depth_phot_trans_lat_60 = np.genfromtxt("output_transit_depth(trans_lat=-59graus,f_spot=0.0).txt", delimiter=",")
+    transit_depth_phot_trans_lat_0 = np.genfromtxt("output_transit_depth(trans_lat=-40graus,f_spot=0.00).txt", delimiter=",")
+    transit_depth_phot_trans_lat_20 = np.genfromtxt("output_transit_depth(trans_lat=-19graus,f_spot=0.00).txt", delimiter=",")
+    transit_depth_phot_trans_lat_40 = np.genfromtxt("output_transit_depth(trans_lat=-40graus,f_spot=0.00).txt", delimiter=",")
+    transit_depth_phot_trans_lat_60 = np.genfromtxt("output_transit_depth(trans_lat=-59graus,f_spot=0.00).txt", delimiter=",")
     transit_depth_phot_trans_lat_70 = np.genfromtxt("output_transit_depth(trans_lat=-70graus,f_spot=0.0).txt", delimiter=",")
     transit_depth_phot_trans_lat_80 = np.genfromtxt("output_transit_depth(trans_lat=-80graus,f_spot=0.0).txt", delimiter=",")
     wavelengths = np.genfromtxt("output_wavelengths.txt", delimiter=",")
@@ -206,7 +206,7 @@ elif (graph == 3):
     #graph3.plot(wavelengths, transit_depth_phot_trans_lat_80,  'o', linestyle='none', markersize=5, color=palette[4])
     #graph3.plot(wavelengths, transit_depth_phot_trans_lat_80, '-', color=palette[5], linewidth=3, label='Trans. Lat. = 80$^{\circ}$')
 
-    plt.text(950, 16000, '$Photosphere$', fontsize=17, bbox=dict(facecolor='white', alpha=0.5))
+    plt.text(950, 15500, '$Photosphere$', fontsize=17, bbox=dict(facecolor='white', alpha=0.5))
     graph3.tick_params(axis="x", direction="in", labelsize=12)
     graph3.tick_params(axis="y", direction="in", labelsize=12)
     plt.subplots_adjust(top=0.9)
@@ -220,11 +220,11 @@ elif (graph == 4):
 
     # Leitura de entrada dos dados
 
-    transit_depth_trans_lat_20_fspot_0 = np.genfromtxt("output_transit_depth(trans_lat=-20graus,f_spot=0.0).txt", delimiter=",")
+    transit_depth_trans_lat_20_fspot_0 = np.genfromtxt("output_transit_depth(trans_lat=-19graus,f_spot=0.00).txt", delimiter=",")
     transit_depth_trans_lat_20_fspot_4 = np.genfromtxt("output_transit_depth(trans_lat=-19graus,f_spot=0.04).txt", delimiter=",")
     transit_depth_trans_lat_46_fspot_0 = np.genfromtxt("output_transit_depth(trans_lat-46graus,f_spot=0.0%).txt", delimiter=",")
     transit_depth_trans_lat_46_fspot_4 = np.genfromtxt("output_transit_depth(trans_lat-46graus,f_spot=4%).txt", delimiter=",")
-    transit_depth_trans_lat_60_fspot_0 = np.genfromtxt("output_transit_depth(trans_lat=-59graus,f_spot=0.0).txt", delimiter=",")
+    transit_depth_trans_lat_60_fspot_0 = np.genfromtxt("output_transit_depth(trans_lat=-59graus,f_spot=0.00).txt", delimiter=",")
     transit_depth_trans_lat_60_fspot_4 = np.genfromtxt("output_transit_depth(trans_lat=-59graus,f_spot=0.04).txt", delimiter=",")
     wavelengths = np.genfromtxt("output_wavelengths.txt", delimiter=",")
 
@@ -287,49 +287,38 @@ elif (graph == 5):
 
     # Leitura de entrada dos dados
 
-    epsilon_Rackham_trans_lat_20_fspot_8 = np.genfromtxt("output_epsilon_Rackham(trans_lat=-20graus,f_spot=0.08).txt", delimiter=",")
-    #epsilon_Rackham_trans_lat_46_fspot_8 = np.genfromtxt("output_epsilon_Rackham(trans_lat-46graus,f_spot=8%).txt", delimiter=",")
-    #epsilon_Rackham_trans_lat_60_fspot_8 = np.genfromtxt("output_epsilon_Rackham(trans_lat=-60graus,f_spot=0.08).txt", delimiter=",")
+    epsilon_Rackham_trans_lat_20_fspot_8 = np.genfromtxt("output_epsilon_Rackham(trans_lat-46graus,f_spot=8%).txt", delimiter=",")
 
     wavelengths = np.genfromtxt("output_wavelengths.txt", delimiter=",")
 
     table_epsilon_ourWork = np.genfromtxt('output_epsilon_fixed_fspot_8% (our work).txt', delimiter=",",
-                                          usecols=(0, 1, 2, 3), skip_header=1)
+                                          usecols=(0, 1, 2, 3, 4, 5), skip_header=1)
     table_epsilon_ourWork = np.transpose(table_epsilon_ourWork)
 
-    palette = sns.color_palette("mako", 4)
+    palette = sns.color_palette("mako", 6)
     graph5 = fig.add_subplot(1, 1, 1)
     #graph5.set_title('WASP-101$\,$b', fontsize=29, fontweight='bold')
     graph5.set_ylabel('Contamination Factor ($\epsilon$)', fontsize=22, fontweight="bold",
                       labelpad=10) # labelpad é a distância entre o título e o eixo
     graph5.set_xlabel('Wavelength (nm)', fontsize=22, fontweight="bold", labelpad=10)
-    #graph5.plot(wavelengths, epsilon,  'o', linestyle='none', markersize=7, color='red', label='$\epsilon$')
-    #graph5.plot(wavelengths, epsilon_Rackham_ff_10,  'o', linestyle='none', markersize=5, color=palette[4])
+
     graph5.plot(wavelengths, epsilon_Rackham_trans_lat_20_fspot_8,  '-', linewidth=3, color='red',
                 label='$\epsilon_{R}$')
-    #graph5.plot(wavelengths, epsilon_Rackham_ff_8,  'o', linestyle='none', markersize=5, color=palette[3])
-    #graph5.plot(wavelengths, epsilon_Rackham_trans_lat_46_fspot_8,  '-', linewidth=3, color=palette[3],
-    #            label='$\epsilon_{R}$ - Trans. Lat. = 46$^{\circ}$')
-    #graph5.plot(wavelengths, epsilon_Rackham_ff_6,  'o', linestyle='none', markersize=5, color=palette[2])
-    #graph5.plot(wavelengths, epsilon_Rackham_trans_lat_60_fspot_8,  '-', linewidth=3, color=palette[1],
-    #            label='$\epsilon_{R}$ - Trans. Lat. = 60$^{\circ}$')
-    #graph5.plot(wavelengths, epsilon_Rackham_ff_4,  'o', linestyle='none', markersize=5, color=palette[1])
-    #graph5.plot(wavelengths, epsilon_Rackham_ff_4,  '-', linewidth=3, color=palette[1],
-    #            label='$\epsilon_{R}$ - f$_{spot}=4\%$')
-    #graph5.plot(wavelengths, epsilon_Rackham_ff_2,  'o', linestyle='none', markersize=5, color=palette[0])
-    #graph5.plot(wavelengths, epsilon_Rackham_ff_2,  '-', linewidth=3, color=palette[0],
-    #            label='$\epsilon_{R}$ - f$_{spot}=2\%$')
+
 
     #graph5.plot(wavelengths, table_epsilon_ourWork[0], '.', linestyle='none', markersize=5, color=palette[0])
-    graph5.plot(wavelengths, table_epsilon_ourWork[3], '-', linewidth=3, color=palette[0],
+    graph5.plot(wavelengths, table_epsilon_ourWork[0], '-', linewidth=3, color=palette[0],
                 label='$\epsilon$ - Trans. Lat. = 0$^{\circ}$')
-    graph5.plot(wavelengths, table_epsilon_ourWork[2], '-', linewidth=3, color=palette[1],
+    graph5.plot(wavelengths, table_epsilon_ourWork[1], '-', linewidth=3, color=palette[1],
+                label='$\epsilon$ - Trans. Lat. = 10$^{\circ}$')
+    graph5.plot(wavelengths, table_epsilon_ourWork[2], '-', linewidth=3, color=palette[2],
                 label='$\epsilon$ - Trans. Lat. = 20$^{\circ}$')
-    graph5.plot(wavelengths, table_epsilon_ourWork[1], '-', linewidth=3, color=palette[2],
-                label='$\epsilon$ - Trans. Lat. = 46$^{\circ}$')
-    graph5.plot(wavelengths, table_epsilon_ourWork[0], '-', linewidth=3, color=palette[3],
-                label='$\epsilon$ - Trans. Lat. = 60$^{\circ}$')
-
+    graph5.plot(wavelengths, table_epsilon_ourWork[3], '-', linewidth=3, color=palette[3],
+                label='$\epsilon$ - Trans. Lat. = 30$^{\circ}$')
+    graph5.plot(wavelengths, table_epsilon_ourWork[4], '-', linewidth=3, color=palette[4],
+                label='$\epsilon$ - Trans. Lat. = 40$^{\circ}$')
+    graph5.plot(wavelengths, table_epsilon_ourWork[5], '-', linewidth=3, color=palette[5],
+                label='$\epsilon$ - Trans. Lat. = 50$^{\circ}$')
 
 
 
